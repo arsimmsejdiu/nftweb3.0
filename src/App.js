88c1c2518eb43aeeb2434fed4f236bb73e88b7cb
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-01-10 23:07:11 +0100

    started buildig the header component

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import './App.css';
 
 function App() {
   return (
-   <Header />
+    <div className='app'>
+      <Header />
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,15 @@
+import "./Header.css"
+import punkLogo from "../assets/header/punkLogo.png"
+
 function Header() {
   return (
-    <div>
-      <h1>Header Component</h1>
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} alt="punk" className="punkLogo"/>
+      </div>
+      <div className="searchBar">
+        <input type="text" placeholder="Search for a beer" className="searchInput"/>
+      </div>
     </div>
   );
 }
