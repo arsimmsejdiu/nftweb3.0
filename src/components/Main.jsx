97933e4bdfbfad8999b94f87799c3ebe601c66c7
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-01-23 15:33:32 +0100

    fixed the design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./Main.css";
+import { shortenAddress } from "../utils/ShortenAddress";
 import instaLogo from "../assets/owner/instagram.png";
 import twitterLogo from "../assets/owner/twitter.png";
 import weth from "../assets/weth.png";
@@ -26,41 +27,43 @@ const Main = ({ punkListData, selectedPunk }) => {
         </div>
         {/* end of punkHighLight div*/}
         <div className="punkDetails" style={{ color: "#fff" }}>
-          <div className="title">{activePunk.name}</div>
-          <div className="eth">
+          <div className="title">
+            {activePunk.name}
             <span className="itemNumber">&#183;#{activePunk.token_id}</span>
+
             <div className="priceContainer">
               <img src={weth} alt="eth" className="wethImage" />
               <div className="priceT"> {activePunk.traits[0]?.value}</div>
             </div>
           </div>
+          <div className="owner">
+            <div className="ownerImageContainer">
+              <img src={activePunk.owner.profile_img_url} alt="owner img" />
+            </div>
+            {/* end of ownerImageContainer   */}
+            <div className="ownerDetails">
+              <div className="ownerNameAndHandle">
+                <div className="address">
+                  {shortenAddress(activePunk.owner.address)}
+                </div>
+                <div className="ownerHandle">@arsimsejdiu</div>
+              </div>
+              {/* end of ownerNameAndHandle div*/}
+              <div className="ownerLink">
+                <img src={instaLogo} alt="instagram" />
+              </div>
+              <div className="ownerLink">
+                <img src={twitterLogo} alt="twitter logo" />
+              </div>
+              <div className="ownerLink">
+                <img src={more} alt="more" />
+              </div>
+            </div>
+            {/* end of ownerDetails div*/}
+          </div>
         </div>
         {/* end of punkDetails   */}
-        <div className="owner">
-          <div className="ownerImageContainer">
-            <img src={activePunk.owner.profile_img_url} alt="owner img" />
-          </div>
-          {/* end of ownerImageContainer   */}
-          <div className="ownerDetails">
-            <div className="ownerNameAndHandle">
-              <div className="address">
-                0x8D67256b9A200A0222B5BdE2C0172596Dc6FFE7B
-              </div>
-              <div className="ownerHandle">@arsimsejdiu</div>
-            </div>
-            {/* end of ownerNameAndHandle div*/}
-            <div className="ownerLink">
-              <img src={instaLogo} alt="instagram" />
-            </div>
-            <div className="ownerLink">
-              <img src={twitterLogo} alt="twitter logo" />
-            </div>
-            <div className="ownerLink">
-              <img src={more} alt="more" />
-            </div>
-          </div>
-          {/* end of ownerDetails div*/}
-        </div>
+
         {/* end of owner div*/}
       </div>
       {/* end of mainContent*/}
