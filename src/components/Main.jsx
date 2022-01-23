d74275e939ef0327a8a78d793e94544e2aea15e4
arsimmsejdiu <arsimmsejdiu@gmail.com>
2022-01-23 15:04:13 +0100

    fixed the design

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./Main.css";
-import king from "../assets/king.png";
 import instaLogo from "../assets/owner/instagram.png";
 import twitterLogo from "../assets/owner/twitter.png";
 import weth from "../assets/weth.png";
-import more from '../assets/owner/more.png';
+import more from "../assets/owner/more.png";
 
 const Main = ({ punkListData, selectedPunk }) => {
   const [activePunk, setActivePunk] = useState(punkListData[0]);
@@ -17,27 +16,39 @@ const Main = ({ punkListData, selectedPunk }) => {
       <div className="mainContent">
         <div className="punkHighLight">
           <div className="punkContainer">
-            <img src={activePunk.image_original_url} alt="active punk" className="selectedPunk" />
-          </div>{/* end of punkContainer */}
-        </div>{/* end of punkHighLight div*/}
+            <img
+              src={activePunk.image_original_url}
+              alt="active punk"
+              className="selectedPunk"
+            />
+          </div>
+          {/* end of punkContainer */}
+        </div>
+        {/* end of punkHighLight div*/}
         <div className="punkDetails" style={{ color: "#fff" }}>
           <div className="title">{activePunk.name}</div>
-          <span className="itemNumber">&#183;#{activePunk.token_id}</span>
-          <div className="priceContainer">
-            <img src={weth} alt="eth" className="wethImage" />
-            <div className="priceT"> {activePunk.traits[0]?.value}</div>
+          <div className="eth">
+            <span className="itemNumber">&#183;#{activePunk.token_id}</span>
+            <div className="priceContainer">
+              <img src={weth} alt="eth" className="wethImage" />
+              <div className="priceT"> {activePunk.traits[0]?.value}</div>
+            </div>
           </div>
-        </div>{/* end of punkDetails   */}
+        </div>
+        {/* end of punkDetails   */}
         <div className="owner">
           <div className="ownerImageContainer">
-            <img src={king} alt="owner img" />
+            <img src={activePunk.owner.profile_img_url} alt="owner img" />
           </div>
           {/* end of ownerImageContainer   */}
           <div className="ownerDetails">
             <div className="ownerNameAndHandle">
-              <div className="address">0x8D67256b9A200A0222B5BdE2C0172596Dc6FFE7B</div>
+              <div className="address">
+                0x8D67256b9A200A0222B5BdE2C0172596Dc6FFE7B
+              </div>
               <div className="ownerHandle">@arsimsejdiu</div>
-            </div>{/* end of ownerNameAndHandle div*/}
+            </div>
+            {/* end of ownerNameAndHandle div*/}
             <div className="ownerLink">
               <img src={instaLogo} alt="instagram" />
             </div>
@@ -47,9 +58,12 @@ const Main = ({ punkListData, selectedPunk }) => {
             <div className="ownerLink">
               <img src={more} alt="more" />
             </div>
-          </div>{/* end of ownerDetails div*/}
-        </div>{/* end of owner div*/}
-      </div>{/* end of mainContent*/}
+          </div>
+          {/* end of ownerDetails div*/}
+        </div>
+        {/* end of owner div*/}
+      </div>
+      {/* end of mainContent*/}
     </div>
   );
 };
